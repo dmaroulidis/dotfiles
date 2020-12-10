b3e0ab2cee6d17f662f198337d9834af09c2617b
Dimitris Maroulidis <dmaroulidis@users.noreply.github.com>
2020-12-10 13:23:52 +0200

    Delete aliases.fish

--- .config/fish/conf.d/aliases.fish
+++
@@ -1,3 +0,0 @@
-alias dotconf="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
-alias docker-cf="sudo docker run -it --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" certbot/dns-cloudflare:latest certonly --dns-cloudflare --dns-cloudflare-credentials /etc/letsencrypt/cloudflare.ini"
-alias co="ykman oath code"
